fb77b2fc3a180f0856955d8ea18d37a72ce16b4e
Roscoe Rubin-Rottenberg <roscoe@knotbin.xyz>
2024-08-24 00:24:37 -0400

    Update index.js

--- components/pages/home/index.js
+++ components/pages/home/index.js
@@ -8,7 +8,6 @@ import config from '@/data/config';
 export default function HomePage(props) {
   return (
     <>
-      <Ribbon onClick={() => window.open(config.links.download)}>Memento is coming on August 20th. Pre-order it on the App Store now!</Ribbon>
       <HeroSection {...props} />
       <IntroFeaturesSection />
     </>
